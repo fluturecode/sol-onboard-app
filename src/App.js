57f811eed450dea48cafc31c8a8b503602443276
Fluturecode <fluturecode@gmail.com>
2024-03-28 13:00:44 -0400

    added dependencies

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import BlockchainPage from './components/BlockchainPage';
+import SolanaPage from './components/SolanaPage';
+import WalletPage from './components/WalletPage';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/blockchain" component={BlockchainPage} />
+        <Route path="/solana" component={SolanaPage} />
+        <Route path="/wallet" component={WalletPage} />
+      </Switch>
+    </Router>
   );
 }
 
